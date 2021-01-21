c2bd101a40d6ae379a187ca00b12f55e4c58ce55
SleepyMan93 <w-donovan@hotmail.co.uk>
2021-01-21 17:15:02 +0000

    JS: Finally got the buttons to fill when their number in the array was called. Changed the start button to a click rather than check. Better for styling and closer to my original design

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,87 @@
 let win;
-let on = false;
 let intervalId;
+let flash;
+let compTurn;
+let order = [];
+let good;
 
 const startButton = document.getElementById('start');
 const turnCounter = document.getElementById('turn');
 
-startButton.addEventListener('click', (event) => {
-    if (startButton.checked == true) {
+let on = false;
+
+/* Start Button */
+
+function startGame () {
+    if (start.value = true) {
         on == true;
-        turnCounter.innerHTML = "-";
-        play ();
+        initialiseGame ();
     } else {
         on == false;
         turnCounter.innerHTML = "";
         clearInterval(intervalId);
     }
-});
+    console.log(start.value);
+}
 
 
-function play() {
-    let win = false;
-    let order = []
-        for (var i = 0; i < 20; i++) {
+function initialiseGame() {
+    win = false;
+    order = [];
+    let playerChoice = [];
+    flash = 0;
+    intervalId = 0;
+    let turn = 1;
+    turnCounter.innerHTML = 1;
+    compTurn = true;
+    good = true
+    for (var i = 0; i < 20; i++) {
             order.push(Math.floor(Math.random() * 5) + 1);
         }
         console.log(order);
-    let playerOrder = [];
-    let flash = 0;
-    let intervalId = 0;
-    let turn = 1;
-    const turnCounter = document.querySelector("#turn")
-        turnCounter.innerHTML = 1;
-    let compTurn = true;
 
-
-    
+    intervalId = setInterval(gameFlash, 800);
 }
+
+function gameFlash() {
+
+        setTimeout(() => {
+            if (order[flash] == 1) one();
+            if (order[flash] == 2) two();
+            if (order[flash] == 3) three();
+            if (order[flash] == 4) four();
+            if (order[flash] == 5) five();
+            flash++;
+        }, 200);
+}
+
+function one() {
+    document.getElementById('pink').style.background = "#F7AEF8";
+}
+
+function two() {
+    document.getElementById('blue').style.background = "#00B2CA";
+}
+
+function three() {
+    document.getElementById('white').style.background = "#E7E6F7";
+}
+
+function four() {
+    document.getElementById('lime').style.background = "#9EE493";
+}
+
+function five() {
+    document.getElementById('orange').style.background = "F79256";
+}
+
+function clearColor() {
+    document.getElementById('pink').style.background = "transparent"
+}
+
+
+
+
+
 
 
